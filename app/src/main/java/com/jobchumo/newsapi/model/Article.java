84173f6b7fe16490da709a22855ba1d989b477cd
jokip <jokipchirchir@gmail.com>
2022-04-04 16:19:46 +0300

    1.0

--- app/src/main/java/com/jobchumo/newsapi/model/Article.java
+++ app/src/main/java/com/jobchumo/newsapi/model/Article.java
@@ -3,6 +3,7 @@ package com.jobchumo.newsapi.model;
 import com.google.gson.annotations.SerializedName;
 
 public class Article {
+
     @SerializedName("title")
     private String title;
     @SerializedName("description")
@@ -13,18 +14,23 @@ public class Article {
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getUrl() {
         return url;
     }
+
     public void setUrl(String url) {
         this.url = url;
     }
